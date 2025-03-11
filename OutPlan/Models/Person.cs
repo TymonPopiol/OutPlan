314c5fc58540855cb648364448ee68b22fe11140
TymonPopiol <popioltymon@gmail.com>
2025-03-11 09:06:37 +0100

    niew models, conectting DB

---
+++ OutPlan/Models/Person.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Person
+{
+    public int Id { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required string Email { get; set; }
+    public DateTime Date_of_birth { get; set; }
+}
